6848ec8969b3b68c9b23a0280ee01411bfc9ebcd
Adam Dominec <adominec@gmail.com>
2021-02-17 14:38:50 +0100

    tidy up code, add comments

--- estimate.py
+++ estimate.py
@@ -6,6 +6,7 @@ from sys import argv
 scale = 15
 
 def find_corners(img):
+    """returns coordinates of maxima in the xy-derivative"""
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY).astype(np.float32)
     gray = cv2.GaussianBlur(gray, (scale, scale), scale/5)
     gray = np.abs(np.diff(np.diff(gray), axis=0)) / 10
@@ -16,6 +17,7 @@ def find_corners(img):
     return centroids[1:].astype(np.float32)
 
 def grid_match(points, directions=4):
+    """returns index to neighbors for each point in grid"""
     flann = cv2.flann_Index()
     flann.build(points, {'algorithm': 1, 'trees': 1})
     best_score = 0
@@ -51,23 +53,24 @@ def order_cross(indices):
         indices[:,1:3] = indices[:,2:0:-1]
 
 def grid_positions(neighbors, directions):
+    """returns integer grid coordinates for each point in grid"""
     result = np.zeros((neighbors.shape[0], directions.shape[1]), np.int32)
     visited = np.zeros((neighbors.shape[0],), np.bool)
     while not visited.all():
         seed = next(i for i, v in enumerate(visited) if not v)
-        boundary = [seed]
-        while boundary:
-            index = boundary.pop()
+        flood = [seed]
+        while flood:
+            index = flood.pop()
             visited[index] = True
             for n, step in zip(neighbors[index,:], directions):
                 if n < 0:
                     continue
                 elif not visited[n]:
                     result[n,:] = result[index] + step
-                    boundary.append(n)
+                    flood.append(n)
                 else:
-                    if not (result[n,:] == result[index] + step).all():
-                        print(index, result[index], "->", n, result[index] + step, "!=", result[n,:])
+                    assert (result[n,:] == result[index] + step).all()
+                    # print(index, result[index], "->", n, result[index] + step, "!=", result[n,:])
     return result
 
 def calibrate(img):
@@ -93,25 +96,3 @@ for fname in argv[2:]:
     img = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
     basename = fname.rsplit("/", 1)[1]
     cv2.imwrite(f"undistorted/{basename}", img)
-
-# print(mtx)
-# print(dist)
-# dist = 2 * dist[0,:] / min(shape)
-# print(f'-fx "ii = i - $cx; jj = j - $cy; xx = $cos*ii +$sin*jj + $cx; yy = -$sin*ii +$cos*jj + $cy; v.p{{xx,yy}}"')
-# print(f'-distort Barrel "{dist[4]} {dist[1]} {dist[0]} 1.0"')
-
-# for (x, y), (i, j, _) in zip(imgpoints, objpoints):
-    # cv2.putText(img, f"{i}, {j}", (round(x), round(y)), cv2.FONT_HERSHEY_PLAIN, 1, (100, 150, 250))
-
-# for x, y in centroids:
-    # cv2.circle(img, (round(x), round(y)), scale//3, (50, 50, 200))
-
-# for (x1, y1), (index,), (distance,) in zip(shifted, indices, distances):
-    # if distance < scale**2:
-        # x2, y2 = points[index]
-        # cv2.line(img, (round(x1), round(y1)), (round(x2), round(y2)), (50, 50, 200))
-
-# img[:-1,:-1,1] += (500*gray).astype(np.uint8)
-
-# cv2.imshow('img', img)
-# cv2.waitKey(0)
